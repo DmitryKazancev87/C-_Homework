da781d1f157dd8e94031aa549df71438c7614687
Dmitry Kazancev <kaz.freeman@bk.ru>
2023-04-12 22:38:28 +0300

    Восстановление после ошибки

--- 3 Task/Program.cs
+++ 3 Task/Program.cs
@@ -1,9 +1,7 @@
 ﻿// 2. Программа, которая на вход принимает число и выдает, является ли число четным или нет.
 Console.WriteLine(" Введите число ");
 int a = int.Parse (Console.ReadLine()!);
-if (a % 2==0) { 
-    Console.WriteLine(" Число четное ");}
-else {    
-Console.WriteLine(" Число не четное ");
-}    
- 
+if (a % 2==0) {
+    Console.WriteLine ("Число четное");}
+else 
+Console.WriteLine ("Число не четное");
