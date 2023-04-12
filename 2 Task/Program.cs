8b9dc2e34be4423e0111ae573d64557289de8e80
Dmitry Kazancev <kaz.freeman@bk.ru>
2023-04-12 21:56:44 +0300

    2 Task completed

---
+++ 2 Task/Program.cs
@@ -0,0 +1,20 @@
+﻿// 2. Программа, которая на вход принимает три числа и выдает, какое число максимальное из этих чисел.
+Console.WriteLine(" Введите первое число ");
+int a = int.Parse (Console.ReadLine()!);
+Console.WriteLine(" Введите второе число ");
+int b = int.Parse (Console.ReadLine()!);
+Console.WriteLine(" Введите третье число ");
+int c = int.Parse (Console.ReadLine()!);
+int max = 0;
+if ((a >= b) && (a >= c)) { 
+    max = a;}
+else if ((b>a) && (b>=c)) { 
+    max = b;}
+else if ((c>=a) && (c>b)) {
+    max = c;}
+else {    
+Console.WriteLine(" Все числа равны, введите разные числа для сравнения ");
+}    
+Console.WriteLine(" Максимальное число ");
+Console.WriteLine(max); 
+ 
