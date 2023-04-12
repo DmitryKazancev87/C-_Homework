7097bbd10ac2fd2ea13fb7513d9907f602ec1a58
Dmitry Kazancev <kaz.freeman@bk.ru>
2023-04-12 22:06:23 +0300

    Task 3 completed.

---
+++ 3 Task/Program.cs
@@ -0,0 +1,9 @@
+﻿// 2. Программа, которая на вход принимает число и выдает, является ли число четным или нет.
+Console.WriteLine(" Введите число ");
+int a = int.Parse (Console.ReadLine()!);
+if (a % 2==0) { 
+    Console.WriteLine(" Число четное ");}
+else {    
+Console.WriteLine(" Число не четное ");
+}    
+ 
